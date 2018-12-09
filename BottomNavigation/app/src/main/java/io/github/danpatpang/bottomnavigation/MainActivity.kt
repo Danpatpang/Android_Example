230746887ea465c2d74a0284086a6b7e73f4e0bd
danpatpang <puri8467@gmail.com>
2018-12-09 23:35:41 +0900

    Change BottomNavigation → TabLayout

--- BottomNavigation/app/src/main/java/io/github/danpatpang/bottomnavigation/MainActivity.kt
+++ BottomNavigation/app/src/main/java/io/github/danpatpang/bottomnavigation/MainActivity.kt
@@ -2,6 +2,7 @@ package io.github.danpatpang.bottomnavigation
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import kotlinx.android.synthetic.main.activity_main.*;
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +12,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        pageAdapter = PageAdapter(supportFragmentManager, 3);
+        tab_layout.addTab(tab_layout.newTab().setText("Home"));
+        tab_layout.addTab(tab_layout.newTab().setText("Analysis"));
+        tab_layout.addTab(tab_layout.newTab().setText("User"));
+        tab_layout.tabGravity = TabLayout.GRAVITY_FILL;
+
+        pageAdapter = PageAdapter(supportFragmentManager, tab_layout.tabCount);
+
         viewpager.adapter = pageAdapter;
+        viewpager.addOnPageChangeListener(
+            TabLayout.TabLayoutOnPageChangeListener(tab_layout)
+        );
+        tab_layout.addOnTabSelectedListener(
+            object : TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab) {
+                    viewpager.currentItem = tab.position;
+                }
+
+                override fun onTabReselected(tab: TabLayout.Tab?) {
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab?) {
+                }
+            }
+        )
     }
 }
--- BottomNavigation/app/src/main/java/io/github/danpatpang/bottomnavigation/PageAdapter.kt
+++ BottomNavigation/app/src/main/java/io/github/danpatpang/bottomnavigation/PageAdapter.kt
@@ -5,6 +5,7 @@ import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
 
 class PageAdapter(fm: FragmentManager, private val numOfTabs: Int) : FragmentStatePagerAdapter(fm) {
+
     override fun getItem(position: Int): Fragment {
         when (position) {
             0 -> {
